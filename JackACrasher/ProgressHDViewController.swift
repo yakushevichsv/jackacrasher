a0f311caea0e81e42c6cf74ae28d5344d0949ead
Siarhei Yakushevich <yakushevichsv@hotmail.com>
2016-03-12 01:53:42 +0300

    JC-SNFriendsRequest: ProgressHDViewController: Issue with centering activity indicator was resolved.

--- JackACrasher/ProgressHDViewController.swift
+++ JackACrasher/ProgressHDViewController.swift
@@ -62,29 +62,37 @@ class ProgressHDViewController: UIViewController {
     
     private func appendBGView() {
         
-        let view = UIView(frame: self.view.bounds)
+        let view1 = UIView(frame: self.view.bounds)
         
-        view.backgroundColor = UIColor.clearColor()
-        view.userInteractionEnabled = false
-        view.translatesAutoresizingMaskIntoConstraints = false
+        view1.backgroundColor = UIColor.clearColor()
+        view1.userInteractionEnabled = false
+        view1.translatesAutoresizingMaskIntoConstraints = false
         
-        let con1 = NSLayoutConstraint(item: self.view, attribute: .Leading, relatedBy: .Equal, toItem: view, attribute: .Leading, multiplier: 1.0, constant: 0)
-        con1.active = true
         
-        let con2 = NSLayoutConstraint(item: self.view, attribute: .Trailing, relatedBy: .Equal, toItem: view, attribute: .Trailing, multiplier: 1.0, constant: 0)
-        con2.active = true
+        self.view.insertSubview(view1, aboveSubview: self.containerView)
+        self.backgroundView = view1
         
-        let con3 = NSLayoutConstraint(item: self.view, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: 0)
-        con3.active = true
         
-        let con4 = NSLayoutConstraint(item: self.view, attribute: .Top, relatedBy: .Equal, toItem: view, attribute: .Top, multiplier: 1.0, constant: 0)
-        con4.active = true
+        if (!view1.translatesAutoresizingMaskIntoConstraints) {
+            let con1 = NSLayoutConstraint(item: self.view, attribute: .Leading, relatedBy: .Equal, toItem: view1, attribute: .Leading, multiplier: 1.0, constant: 0)
+            con1.active = true
+            
+            let con2 = NSLayoutConstraint(item: self.view, attribute: .Trailing, relatedBy: .Equal, toItem: view1, attribute: .Trailing, multiplier: 1.0, constant: 0)
+            con2.active = true
+            
+            let con3 = NSLayoutConstraint(item: self.view, attribute: .Bottom, relatedBy: .Equal, toItem: view1, attribute: .Bottom, multiplier: 1.0, constant: 0)
+            con3.active = true
+            
+            let con4 = NSLayoutConstraint(item: self.view, attribute: .Top, relatedBy: .Equal, toItem: view1, attribute: .Top, multiplier: 1.0, constant: 0)
+            con4.active = true
         
-        self.view.insertSubview(view, aboveSubview: self.containerView)
+            view1.superview?.setNeedsLayout()
+        }
+        else {
+            
+            view1.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.FlexibleWidth.rawValue | UIViewAutoresizing.FlexibleHeight.rawValue )
+        }
         
-        self.view.setNeedsLayout()
-        
-        self.backgroundView = view
         
     }
     
@@ -94,25 +102,35 @@ class ProgressHDViewController: UIViewController {
         
         let frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
         
-        let view = UIView(frame:frame)
-        view.backgroundColor = UIColor.blackColor()
-        view.alpha = 0.8
-        view.layer.cornerRadius = 10
-        view.layer.masksToBounds = true
-        //view.center = self.view.bounds.center
-        view.translatesAutoresizingMaskIntoConstraints = false
+        let view1 = UIView(frame:frame)
+        view1.backgroundColor = UIColor.blackColor()
+        view1.alpha = 0.8
+        view1.layer.cornerRadius = 10
+        view1.layer.masksToBounds = true
+        assert(!CGRectEqualToRect(self.view.bounds, CGRectZero))
+        view1.center = self.view.bounds.center
+        view1.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(view)
-        self.containerView = view
+        self.view.addSubview(view1)
+        self.containerView = view1
         
-        let const1 = view.centerXAnchor.constraintEqualToAnchor(self.view.centerXAnchor)
-        const1.active = true
+        if (!view1.translatesAutoresizingMaskIntoConstraints) {
+            let const1 = view1.centerXAnchor.constraintEqualToAnchor(self.view.centerXAnchor)
         
-        let const2 = view.centerYAnchor.constraintEqualToAnchor(self.view.centerYAnchor)
-        const2.active = true
+            let const2 = view1.centerYAnchor.constraintEqualToAnchor(self.view.centerYAnchor)
         
-        NSLayoutConstraint.activateConstraints([const1,const2])
-        
+            let const3 = view1.widthAnchor.constraintEqualToConstant(size.width)
+            
+            let const4 = view1.heightAnchor.constraintEqualToConstant(size.height)
+            
+            NSLayoutConstraint.activateConstraints([const1,const2,const3,const4])
+            
+            
+        }
+        else {
+            view1.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.FlexibleLeftMargin.rawValue | UIViewAutoresizing.FlexibleRightMargin.rawValue |
+                UIViewAutoresizing.FlexibleTopMargin.rawValue | UIViewAutoresizing.FlexibleBottomMargin.rawValue )
+        }
         
     }
     
@@ -120,26 +138,35 @@ class ProgressHDViewController: UIViewController {
     private func appendActivityIndicator() {
         
         assert(self.containerView != nil)
-        let view = UIActivityIndicatorView(activityIndicatorStyle: .White)
-        view.translatesAutoresizingMaskIntoConstraints = false
+        let view1 = UIActivityIndicatorView(activityIndicatorStyle: .White)
+        view1.translatesAutoresizingMaskIntoConstraints = false
         
-        view.hidesWhenStopped = true
+        view1.hidesWhenStopped = true
         
-        view.backgroundColor = UIColor.blackColor()
-        view.alpha = 0.8
-        view.layer.cornerRadius = 10
-        view.layer.masksToBounds = true
-        //view.center = self.containerView.bounds.center
+        view1.backgroundColor = UIColor.blackColor()
+        view1.alpha = 0.8
+        view1.layer.cornerRadius = 10
+        view1.layer.masksToBounds = true
+        view1.center = self.containerView.bounds.center
+        assert(!CGRectEqualToRect(self.containerView.frame, CGRectZero))
         
-        self.containerView.addSubview(view)
-        self.activityIndicator = view
+        self.containerView.addSubview(view1)
+        self.activityIndicator = view1
         
         
-        let const1 = view.centerXAnchor.constraintEqualToAnchor(self.containerView.centerXAnchor)
-        
-        let const2 = view.centerYAnchor.constraintEqualToAnchor(self.containerView.centerYAnchor)
-        
-        NSLayoutConstraint.activateConstraints([const1,const2])
+        if (!view1.translatesAutoresizingMaskIntoConstraints) {
+            let const1 = view1.centerXAnchor.constraintEqualToAnchor(self.containerView.centerXAnchor)
+            
+            let const2 = view1.centerYAnchor.constraintEqualToAnchor(self.containerView.centerYAnchor)
+            
+            NSLayoutConstraint.activateConstraints([const1,const2])
+            
+            self.containerView.layoutIfNeeded()
+        }
+        else {
+            view1.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.FlexibleLeftMargin.rawValue | UIViewAutoresizing.FlexibleRightMargin.rawValue |
+                UIViewAutoresizing.FlexibleTopMargin.rawValue | UIViewAutoresizing.FlexibleBottomMargin.rawValue )
+        }
         
         
         
@@ -174,6 +201,8 @@ class ProgressHDViewController: UIViewController {
     }
     
     func hideProgress(animated:Bool = false ) {
+        
+        return
         
         if self.containerView == nil || self.containerView.hidden == true {
             return
